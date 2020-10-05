28fee9888dc913289abda107763bcac29d26d275
usamaeltmsah <usamaeltmsah@gmail.com>
2020-10-06 00:59:48 +0200

    Find the new move positions for a position

--- knight_path_finder.rb
+++ knight_path_finder.rb
@@ -1,15 +1,38 @@
 require_relative 'poly_tree_node'
 
 BOARD_SIZE = 8
+MOVES = [
+    [-1, -2],
+    [-2, -1],
+    [-1, 2],
+    [-2, 1],
+    [1, -2],
+    [2, -1],
+    [1, 2],
+    [2, 1]
+]
 
 class KnightPathFinder
     attr_reader :root_node
     def initialize(initial_pos)
         @initial_pos = initial_pos
         @root_node = PolyTreeNode.new(@initial_pos)
+        @considered_positions = [initial_pos]
     end
 
     def self.valid_moves(pos)
         pos.all? { |cord| cord.between?(0, BOARD_SIZE-1) }
+    end
+
+    def new_move_positions(pos)
+        row, col = pos
+        new_positions = []
+        MOVES.each do |dx, dy|
+            new_pos = [row + dx, col + dy]
+            if KnightPathFinder.valid_moves(new_pos)
+                new_positions << new_pos && @considered_positions << new_pos unless @considered_positions.include?(new_pos)
+            end
+        end
+        new_positions
     end
 end
