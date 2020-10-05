d9aa2e5267a21308edfd22bea5c7720159d2305e
usamaeltmsah <usamaeltmsah@gmail.com>
2020-10-05 17:49:19 +0200

    Check is this position is a valid position or not

--- knight_path_finder.rb
+++ knight_path_finder.rb
@@ -1,9 +1,15 @@
 require_relative 'poly_tree_node'
 
+BOARD_SIZE = 8
+
 class KnightPathFinder
     attr_reader :root_node
     def initialize(initial_pos)
         @initial_pos = initial_pos
         @root_node = PolyTreeNode.new(@initial_pos)
+    end
+
+    def self.valid_moves(pos)
+        pos.all? { |cord| cord.between?(0, BOARD_SIZE-1) }
     end
 end
