13c6f77456984826288e9f9b892b04194fb8a2e0
usamaeltmsah <usamaeltmsah@gmail.com>
2020-10-05 17:31:03 +0200

    Call PolyTreeNode in the right way

--- knight_path_finder.rb
+++ knight_path_finder.rb
@@ -1,6 +1,9 @@
+require_relative 'poly_tree_node'
+
 class KnightPathFinder
+    attr_reader :root_node
     def initialize(initial_pos)
         @initial_pos = initial_pos
-        @@root_node = PoltTreeNode(@initial_pos)
+        @root_node = PolyTreeNode.new(@initial_pos)
     end
 end
